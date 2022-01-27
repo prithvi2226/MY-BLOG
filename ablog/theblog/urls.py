fb8c153792e688acb4f4bdf3e53518d89b776b47
prithvi2226 <itsmeprithvi2000@gmail.com>
2022-01-28 02:36:57 +0530

    added delete function plus latest post on top

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
 
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
 	path('article/<int:pk>', ArticleDetailView.as_view(), name = "article-detail"),
 	path('add_post/', AddPostView.as_view(), name = 'add_post'),
 	path('article/edit/<int:pk>', UpdatePostView.as_view(), name = 'update_post'),
-
+	path('article/<int:pk>/remove', DeletePostView.as_view(), name = 'delete_post'),
 ]
+
+
